6629c0afb786725dd67304c03d9e965f79d1246f
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-07-02 17:32:24 +0100

    Added slider component, dummy data and slider functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Announcement />
+      <Navbar />
+      <Slider />
+    </>
+  );
 };
 
 export default Home;
